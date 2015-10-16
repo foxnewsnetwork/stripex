abea0bf8d86987c73fb1c35821eb9ef1838cd527
Anatolii Domaratskyi <domaratskyias@outlook.com>
2015-10-16 21:00:39 +0300

    fix castomer attrs: card -> source

--- lib/stripex/cards.ex
+++ lib/stripex/cards.ex
@@ -1,7 +1,7 @@
 defmodule Stripex.Cards do
   @resource Stripex.Card
-  @endpoint "/v1/customers/:customer_id/cards/:id"
+  @endpoint "/v1/customers/:customer_id/sources/:id"
   use Gateway.Resource, update: :post
-  
+
   def all_type, do: %{"data" => [@resource]}
 end
